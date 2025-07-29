095ae1cfbfc2ee1798d9c6c5dfbb9cba2bf4afce
Mateusz Baginski <cziken58@gmail.com>
2025-07-29 10:19:41 +0200

    fix: dialyzer

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,3 +1,11 @@
 [
-  { "deps/phoenix/lib/phoenix/router.ex", :pattern_match, 1 }
+  {"deps/phoenix/lib/phoenix/router.ex", :pattern_match, 1},
+  {"test/support/preset_asserations_case_template.ex", :unknown_function,
+   "ExUnit.Callbacks.on_exit/1"},
+  {"test/support/preset_asserations_case_template.ex", :unknown_function,
+   "ExUnit.CaseTemplate.__proxy__/2"},
+  {"test/support/preset_asserations_case_template.ex", :unknown_function,
+   "ExUnit.Callbacks.__merge__/4"},
+  {"test/support/preset_asserations_case_template.ex", :unknown_function,
+   "ExUnit.Callbacks.__noop__/0"}
 ]

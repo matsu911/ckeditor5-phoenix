d9c3f175ce5f0108d087cbb91db021e87fe3254f
Mateusz Baginski <cziken58@gmail.com>
2025-08-10 17:44:38 +0200

    fix: re-export `unwrapEditorWatchdog`

--- npm_package/src/index.ts
+++ npm_package/src/index.ts
@@ -1,3 +1,4 @@
 export { Hooks } from './hooks';
 export { CustomEditorPluginsRegistry } from './hooks/editor/custom-editor-plugins';
 export { EditorsRegistry } from './hooks/editor/editors-registry';
+export { unwrapEditorWatchdog } from './hooks/editor/utils/wrap-with-watchdog';
